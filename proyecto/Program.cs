7b1c4ea5542c60e9da41bf7c805d75f2bd4385e3
ixaac ramirez <isaacramirezm2004@gmail.com>
2024-08-15 13:35:03 -0600

    actualicacion  en la clase progra

--- proyecto/Program.cs
+++ proyecto/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola Mundo");
+
+public class progra
+{
+    public static string EdadMeses { get; private set; }
+
+    static void main(string[] Args)
+    {
+        int Edad;
+        string Nombre;
+        Console.WriteLine("bienvenidos a nuestro primer sistema cread en c#");
+        Console.WriteLine("ingrese su nombre para continuar");
+        Nombre= Console.ReadLine();//para recibir valores de caracter
+        Console.WriteLine("ingrese su edad");
+        Edad= Convert.ToInt32(Console.ReadLine);//conbertir a datos tipo double
+        EdadMeses= Edad * 12;// calculos Matematicos 
+        Console.WriteLine("tu edad en meses es: " + EdadMeses + "Meses");//concatenacion 
+    }
+} 
